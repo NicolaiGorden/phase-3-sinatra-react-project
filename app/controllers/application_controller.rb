8c52acdb2fad602712cdbbad724da6c5a292cc56
Nicolai <Nicolaigorden@gmail.com>
2023-02-13 22:57:31 -0500

    post request interacts with frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,26 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  get '/' do
+    "Hello world"
+  end
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/mons' do
+    mons = Mon.all.order(:id).reverse
+    mons.to_json
+  end
+  
+  post '/mons' do
+    mon = Mon.create(
+      nickname: params[:nickname],
+      species: params[:species],
+      ability: params[:ability],
+      move1: params[:move1],
+      move2: params[:move2],
+      move3: params[:move3],
+      move4: params[:move4]
+    )
+    mon.to_json
   end
 
 end
